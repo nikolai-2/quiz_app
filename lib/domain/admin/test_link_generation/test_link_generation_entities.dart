ea382333b4d510da40bffdabe48c8df4500c1ef5
Ilya Beregovskiy <festeloqq@gmail.com>
2021-10-24 15:29:00 +0300

    xyi

---
+++ lib/domain/admin/test_link_generation/test_link_generation_entities.dart
@@ -0,0 +1,17 @@
+class Employee {
+  final String id;
+  final String name;
+  Employee({
+    required this.id,
+    required this.name,
+  });
+}
+
+class Candidate {
+  final String id;
+  final String name;
+  Candidate({
+    required this.id,
+    required this.name,
+  });
+}
--- lib/domain/admin/test_link_generation/test_link_generation_repository.dart
+++ lib/domain/admin/test_link_generation/test_link_generation_repository.dart
@@ -1,7 +1,13 @@
+import 'package:new_app/domain/admin/employees_list/employee_list_entities.dart';
+
 abstract class TestLinkGenerationRepository {
   Future<void> generateTestForEmployee(String testId, String employeeId);
 
   Future<void> generateTestForCandidate(String testId, String candidateId);
 
   Future<void> generateTestForNewCandidate(String testId, String name);
+
+  Future<List<Employee>> getEmployesForTesting();
+
+  Future<List<Employee>> getCandidatesForTesting();
 }
