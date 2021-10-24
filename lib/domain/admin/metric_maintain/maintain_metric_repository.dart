7e93dd773c591976afcee1f98f93681f7113b94a
Ilya Beregovskiy <festeloqq@gmail.com>
2021-10-24 13:38:50 +0300

    go v n o

--- lib/domain/admin/metric_maintain/maintain_metric_repository.dart
+++ lib/domain/admin/metric_maintain/maintain_metric_repository.dart
@@ -3,5 +3,7 @@ import 'package:new_app/domain/admin/metric_maintain/maintain_metric_entities.da
 abstract class MaintainMetricRepository {
   Future<void> saveMetric(MaintainableMetric metric);
 
+  Future<MaintainableMetric> getMetric(String id);
+
   Future<void> deleteMetric(MaintainableMetric metric);
 }
