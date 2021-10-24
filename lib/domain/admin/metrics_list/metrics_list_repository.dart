2c57b7feadd8ed3f64b0debf05d537d8e8124322
Ilya Beregovskiy <festeloqq@gmail.com>
2021-10-24 13:42:01 +0300

    g o v n o

--- lib/domain/admin/metrics_list/metrics_list_repository.dart
+++ lib/domain/admin/metrics_list/metrics_list_repository.dart
@@ -1,5 +1,5 @@
 import 'package:new_app/domain/admin/metrics_list/metrics_list_entities.dart';
 
 abstract class MetricListRepository {
-  Future<List<Metric>> getTests(Metric metric);
+  Future<List<Metric>> getMetrics();
 }
